50573599a8545ddc0b200b5dc9b187edeeb759bb
NirmalChandra21 <121340069+NirmalChandra21@users.noreply.github.com>
2024-01-21 15:23:22 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        title: "Natours",
+        description: "Landing page for a fictional nature tourism company.",
+        tools: "scss (focuses on floats)",
+        img: "",
+        link: "",
+        type: "learning projects",
+    },
+
+    {
+        title: "Guess My Number",
+        description: "Try to guess a randomly generated number.",
+        tools: "css, javascript",
+        img: "",
+        link: "https://zeld-guess-my-number.netlify.app/",
+        type: "learning projects",
+    },
+
+    {
+        title: "Pig Game",
+        description: "The popular dice game played with two players.",
+        tools: "css, javascript",
+        img: "https://raw.githubusercontent.com/Peac-h/0_learning_projects/main/pig_game.png",
+        link: "https://zeld-pig-game.netlify.app/",
+        type: "learning projects",
+    },
+    {
+        title: "Bankist Page",
+        description: "Fictional Bank Webpage.",
+        tools: "css, javascript (focuses on advanced DOM manipulation)",
+        img: "",
+        link: "https://zeld-bankist-app.netlify.app/",
+        type: "learning projects",
+    },
+];
---
+++ js/view/heroView.js
@@ -0,0 +1,42 @@
+import View from "./view.js";
+
+class HeroView extends View {
+    _date = this._selectId("currDateIndia");
+
+    _reloadBtn = this._selectId("btnReload");
+
+    addReloadButtons(handler) {
+        this._addEventListeners(this._reloadBtn, "click", handler, {
+            passive: true,
+        });
+    }
+
+    _renderDate() {
+        const currDate = new Intl.DateTimeFormat("ka-GE", {
+            hour: "numeric",
+            minute: "numeric",
+            day: "numeric",
+            month: "numeric",
+        }).format(new Date());
+
+        this._date.innerHTML = `${currDate} in India`;
+    }
+
+    async _renderHero() {
+        const spans = document.querySelectorAll(".introductory-text-span");
+
+        const items = document.querySelectorAll(".animated-item--translateX");
+
+        for (const span of spans) {
+            await new Promise((resolve) => setTimeout(resolve, 100));
+            span.classList.remove("animated-item--translateY");
+        }
+
+        for (const item of items) {
+            await new Promise((resolve) => setTimeout(resolve, 100));
+            item.classList.remove("animated-item--translateX");
+        }
+    }
+}
+
+export default new HeroView();
